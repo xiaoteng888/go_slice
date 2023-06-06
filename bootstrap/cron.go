f2966dc5eed0184cede84f1b9150f0c621043764
KoKo <123456@qq.com>
2023-06-06 12:34:05 +0800

    互斥锁互相独立

--- bootstrap/cron.go
+++ bootstrap/cron.go
@@ -11,7 +11,9 @@ import (
 	cronV3 "github.com/robfig/cron/v3"
 )
 
-var mutex sync.Mutex
+var mutex1 sync.Mutex
+var mutex2 sync.Mutex
+var mutex3 sync.Mutex
 
 func SetupCron() {
 
@@ -28,11 +30,11 @@ func SetupCron() {
 			}
 		}()
 		fmt.Println("\n定时任务-扫描视频：每300秒执行一次", time.Now().Format("2006-01-02 15:04:05"))
-		// 获取互斥锁
-		mutex.Lock()
+		// 获取互斥锁1
+		mutex1.Lock()
+		defer mutex1.Unlock()
 		vc.SaveToMysql()
-		// 释放互斥锁
-		mutex.Unlock()
+
 	})
 
 	// 执行切片
@@ -46,11 +48,10 @@ func SetupCron() {
 			}
 		}()
 		fmt.Println("\n定时任务-切片上传S3：每600秒执行一次", time.Now().Format("2006-01-02 15:04:05"))
-		// 获取互斥锁
-		mutex.Lock()
+		// 获取互斥锁2
+		mutex2.Lock()
+		defer mutex2.Unlock()
 		vc.DoSlice()
-		// 释放互斥锁
-		mutex.Unlock()
 	})
 
 	go c.AddFunc("0 20 * * *", func() {
@@ -63,11 +64,10 @@ func SetupCron() {
 			}
 		}()
 		fmt.Println("\n定时任务-切片昨天切片中的视频：每天8点执行一次", time.Now().Format("2006-01-02 15:04:05"))
-		// 获取互斥锁
-		mutex.Lock()
+		// 获取互斥锁3
+		mutex3.Lock()
+		defer mutex3.Unlock()
 		vc.DoYestedaySlice()
-		// 释放互斥锁
-		mutex.Unlock()
 	})
 	c.Start()
 	// 程序运行保持不退出
